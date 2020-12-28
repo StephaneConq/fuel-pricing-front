38d55a93266acbe19cb67d161140fad7984c22d8
Stéphane Conq <conqstephane@gmail.com>
2020-12-28 12:06:12 +0100

    sort data when moving map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -117,6 +117,7 @@ export class MapComponent implements OnInit, AfterViewInit {
     });
     if (this.listInstance) {
       this.listInstance.instance.data = [...this.displayedStations];
+      this.listInstance.instance.sortData();
     }
   }
 
